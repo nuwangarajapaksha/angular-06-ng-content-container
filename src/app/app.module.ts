a48325f27d80219c3940018ccb2c40ea82d55c30
Nuwanga Rajapaksha <57533167+nuwangarajapaksha@users.noreply.github.com>
2023-07-08 00:46:31 +0530

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PanelComponent } from './panel/panel.component';
+
+@NgModule({
+  declarations: [AppComponent, PanelComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/panel/panel.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+// 01 ngContent,ngContainer
+@Component({
+  selector: 'bootstrap-panel',
+  templateUrl: './panel.component.html',
+  styleUrls: ['./panel.component.css'],
+})
+export class PanelComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
